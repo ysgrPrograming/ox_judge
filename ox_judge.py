3232c58ce45ef1ddf77cc5a65c8691e5e4cc19d2
ysgrPrograming <59981708+ysgrPrograming@users.noreply.github.com>
2021-07-06 06:01:14 +0900

    Update ox_judge.py

--- ox_judge.py
+++ ox_judge.py
@@ -16,7 +16,7 @@ def convert_nto10(x, n):
     return sum([x[i]*n**i for i in range(len(x))])
 
 #既に勝敗が決まっている場合評価値を出力する
-#出力 = (決着がついているか(bool), 評価値(int))
+#戻り値 = (決着がついているか(bool), 評価値(int))
 check_li = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]
 def check_bingo(x):
     li = convert_10ton(x, 3)
@@ -54,56 +54,7 @@ def print_t(t):
         print(*new_li[i*3:i*3+3])
     print()
 
-#scenesリストに評価値を格納し、is_confirmedリストに評価値が確定済みか記録する
-scenes = [0]*(scene_size*2)
-is_confirmed = [False]*(scene_size*2)
-for i in range(scene_size*2):
-    if is_confirmed[i]: continue
-    todo = deque([i])
-    while todo:
-        t = todo.pop()
-        if is_confirmed[t]: continue
-        #print_t(t)
 
-        #局面の勝敗が既についている時
-        c = check_bingo(t)
-        if c[0]:
-            scenes[t] = c[1]
-            is_confirmed[t] = True
-            #print("confirmed", scenes[t])
-            continue
-
-        #局面の勝敗がついていない時
-        turn = t//scene_size
-        li = convert_10ton(t%scene_size, 3)
-        scene_val = 0
-        if turn == 0:
-            scene_val = -float('inf')
-        else:
-            scene_val = float('inf')
-
-        #次の局面としてありえる局面の番号をnew_scenesに格納する
-        #既に評価済みの場合、ミニマックス法に基づいて現局面の評価値を計算する
-        new_scenes = list_next_scenes(t)
-        for scene in new_scenes:
-            if is_confirmed[scene]:
-                if turn == 0:
-                    scene_val = max(scene_val, scenes[scene])
-                else:
-                    scene_val = min(scene_val, scenes[scene])
-            else:
-                break
-
-        else:
-            #現局面を評価可能の場合、評価する
-            scenes[t] = scene_val/2
-            is_confirmed[t] = True
-            #print("confirmed", scenes[t])
-
-        #未探索の局面がある場合、探索を続行する
-        if is_confirmed[t] == False:
-            new_scenes.appendleft(t)
-            todo.extend(new_scenes)
 
 #入力
 turn_dic = {"p": 0, "e": 1}
@@ -112,31 +63,81 @@ data_list = []
 for i in range(3):
     part = list(map(int, input().split(' ')))
     data_list.extend(part)
-scene = convert_nto10(data_list, 3)+turn*scene_size
+start_scene = convert_nto10(data_list, 3)+turn*scene_size
 
-#出力
-#必勝の有無、手数を宣言
-if scenes[scene] < 0:
-    num = int(-math.log2(abs(scenes[scene])))
-    print("Enemy wins in", num, "moves")
-elif scenes[scene] == 0:
-    print("Draw")
-else:
-    num = int(-math.log2(abs(scenes[scene])))
-    print("Player wins in", num, "moves")
+#scenesリストに評価値を格納し、is_confirmedリストに評価値が確定済みか記録する
+scenes = [0]*(scene_size*2)
+is_confirmed = [False]*(scene_size*2)
+todo = deque([start_scene])
+while todo:
+    t = todo.pop()
+    if is_confirmed[t]: continue
+    #print_t(t)
 
-#手順
-print_t(scene)
-while check_bingo(scene)[0] == False:
+    #局面の勝敗が既についている時
+    c = check_bingo(t)
+    if c[0]:
+        scenes[t] = c[1]
+        is_confirmed[t] = True
+        #print("confirmed", scenes[t])
+        continue
+
+    #局面の勝敗がついていない時
+    turn = t//scene_size
+    li = convert_10ton(t%scene_size, 3)
     scene_val = 0
-    scene_num = 0
-    turn = scene//scene_size
     if turn == 0:
         scene_val = -float('inf')
     else:
         scene_val = float('inf')
 
-    li = list_next_scenes(scene)
+    #次の局面としてありえる局面の番号をnew_scenesに格納する
+    #既に評価済みの場合、ミニマックス法に基づいて現局面の評価値を計算する
+    new_scenes = list_next_scenes(t)
+    for scene in new_scenes:
+        if is_confirmed[scene]:
+            if turn == 0:
+                scene_val = max(scene_val, scenes[scene])
+            else:
+                scene_val = min(scene_val, scenes[scene])
+        else:
+            break
+
+    else:
+        #現局面を評価可能の場合、評価する
+        scenes[t] = scene_val/2
+        is_confirmed[t] = True
+        #print("confirmed", scenes[t])
+
+    #未探索の局面がある場合、探索を続行する
+    if is_confirmed[t] == False:
+        new_scenes.appendleft(t)
+        todo.extend(new_scenes)
+
+#出力
+#必勝の有無、手数を宣言
+if scenes[start_scene] < 0:
+    num = int(-math.log2(abs(scenes[start_scene])))
+    print("Enemy wins in", num, "moves")
+elif scenes[start_scene] == 0:
+    print("Draw")
+else:
+    num = int(-math.log2(abs(scenes[start_scene])))
+    print("Player wins in", num, "moves")
+
+#手順の出力
+print_t(start_scene)
+trans_scene = start_scene
+while check_bingo(trans_scene)[0] == False:
+    scene_val = 0
+    scene_num = 0
+    turn = trans_scene//scene_size
+    if turn == 0:
+        scene_val = -float('inf')
+    else:
+        scene_val = float('inf')
+
+    li = list_next_scenes(trans_scene)
     for i in li:
         if turn == 0:
             if scenes[i] > scene_val:
@@ -146,11 +147,11 @@ while check_bingo(scene)[0] == False:
             if scenes[i] < scene_val:
                 scene_val = scenes[i]
                 scene_num = i
-    scene = scene_num
+    trans_scene = scene_num
     if turn == 0:
         print("Player turn:")
     else:
         print("Enemy turn:")
-    print_t(scene)
+    print_t(trans_scene)
 
 print("end")
